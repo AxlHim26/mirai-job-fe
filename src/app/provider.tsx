2cd98dd50430b4be8437d3142d8925ba7bb061e6
TieByn26 <dxuantienn@gmail.com>
2025-06-19 18:51:59 +0700

    build(app): bootstrap the initial frontend architecture

---
+++ src/app/provider.tsx
@@ -0,0 +1,41 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as React from 'react';
+
+import { Spinner } from '@/components/ui/spinner';
+import { AuthLoader } from '@/lib/auth';
+import { queryConfig } from '@/lib/react-query';
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      }),
+  );
+
+  return (
+    <React.Suspense
+      fallback={
+        <div className="flex h-screen w-screen items-center justify-center">
+          <Spinner size="xl" />
+        </div>
+      }
+    >
+          <QueryClientProvider client={queryClient}>
+            <AuthLoader
+              renderLoading={() => (
+                <div className="flex h-screen w-screen items-center justify-center">
+                  <Spinner size="xl" />
+                </div>
+              )}
+            >
+              {children}
+            </AuthLoader>
+          </QueryClientProvider>
+    </React.Suspense>
+  );
+};
---
+++ src/app/routes/auth/register.tsx
@@ -0,0 +1,12 @@
+
+const RegisterRoute = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <p>Please register to create an account.</p>
+      {/* Add your registration form or component here */}
+    </div>
+  );
+}
+
+export default RegisterRoute;
