830b3a46a1d16ee6a22b033bac10f71cbc638086
lxtuyen <127100115+lxtuyen@users.noreply.github.com>
2025-07-03 22:02:19 +0700

    feat: ui browseCompanies #2

--- src/app/routes/app/public/browse-companies.tsx
+++ src/app/routes/app/public/browse-companies.tsx
@@ -64,7 +64,7 @@ const BrowseCompaniesRoute = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-start w-full h-full bg-[#F8F9FC] max-w-[2000px]">
+    <>
       {/* Header Section */}
       <Header />
       
@@ -130,7 +130,7 @@ const BrowseCompaniesRoute = () => {
       </div>
       {/* Footer Section */}
       <Footer />
-    </div>
+    </>
   );
 };
 
