5defc68de7fb29c3c949adffc805124e587c22d1
lxtuyen <127100115+lxtuyen@users.noreply.github.com>
2025-07-03 21:56:33 +0700

    feat(ui): build browse-companies page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,7 @@
 import auth_bg from "./auth-bg.png";
+import heroImage from './hero-heading.png';
+import companyLogo from './Nomad.png';
+import desktop from './desktop.png';
+import socialMedia from './social-media.png';
 
-export {auth_bg};
+export { heroImage, companyLogo, desktop, socialMedia, auth_bg };
