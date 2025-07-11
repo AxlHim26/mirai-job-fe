52d5e9a8020770d88a9158427762cc649b190a91
JunCodera1 <minhtien0601200@gmail.com>
2025-07-11 15:14:32 +0700

    docs(card): revise CompanyCard and CompanyResultCard stories

--- src/components/ui/card/CompanyResultCard.stories.tsx
+++ src/components/ui/card/CompanyResultCard.stories.tsx
@@ -1,9 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
 import CompanyResultCard, { CompanyCardProps } from "./CompanyResultCard";
 
-export default {
+const meta: Meta<typeof CompanyResultCard> = {
   title: "Components/CompanyResultCard",
   component: CompanyResultCard,
+  parameters: {
+    docs: {
+      description: {
+        component: "Displays company information in card format including name, logo and number of jobs.",
+      },
+    },
+  },
+  tags: ["autodocs"],
 };
+
+export default meta;
+
+type Story = StoryObj<typeof CompanyResultCard>;
 
 const sampleData: CompanyCardProps = {
   id: 1,
@@ -12,4 +25,6 @@ const sampleData: CompanyCardProps = {
   jobsQuantity: 7,
 };
 
-export const Default = () => <CompanyResultCard {...sampleData} />;
+export const Default: Story = {
+  args: sampleData,
+};
