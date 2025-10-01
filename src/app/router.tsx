ad44facd99fde95d8adf4866c39247efaeca0400
Đặng Xuân Tiến <147963636+AxlHim26@users.noreply.github.com>
2025-10-01 11:18:43 +0700

    Update recruiter route import to use RecruiterRoute

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -68,7 +68,7 @@ export const createRouter = (queryClient: QueryClient) => {
         {
           path: 'recruiter', // layout for recruiter
           lazy: () =>
-            import('./routes/app/private/recruiter/recruiter').then(withClient),
+            import('./routes/app/private/recruiter/RecruiterRoute').then(withClient),
         },
         {
           path: 'dashboard',
