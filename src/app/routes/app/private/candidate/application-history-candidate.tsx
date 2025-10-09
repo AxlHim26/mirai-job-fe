8f847d8a63a2a846f2b5e0c436a9bdf50fbdeee7
JunCodera1 <minhtien0601200@gmail.com>
2025-10-09 09:12:22 +0700

    feat(candidate/application): added layout and some component of application history

---
+++ src/app/routes/app/private/candidate/application-history-candidate.tsx
@@ -0,0 +1,31 @@
+import { Pagination } from "@/components/ui/pagination/pagination";
+import { ApplicationControls } from "@/features/application-history/controls";
+import { ApplicationFeatureBanner } from "@/features/application-history/feature-banner";
+import { ApplicationHeader } from "@/features/application-history/header";
+import { ApplicationTables } from "@/features/application-history/tables";
+import { ApplicationTabs } from "@/features/application-history/tabs";
+
+export const ApplicationHistoryCandidate = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 ">
+      <div className="max-w-7xl mx-auto">
+        <ApplicationHeader />
+        <ApplicationFeatureBanner />
+        <ApplicationTabs />
+        <div className="bg-white rounded-b-lg shadow-sm">
+          <ApplicationControls />
+          <ApplicationTables />
+          <div className="flex justify-center mt-4">
+            <Pagination
+              currentPage={1}
+              totalPages={5}
+              onPageChange={() => {}}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationHistoryCandidate;
---
+++ src/features/application-history/index.ts
@@ -0,0 +1,5 @@
+export * from "./header";
+export * from "./feature-banner";
+export * from "./tables";
+export * from "./tabs";
+export * from "./controls";
